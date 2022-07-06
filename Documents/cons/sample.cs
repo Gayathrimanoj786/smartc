694f9695b1265971221f6daf5c711f45307acb08
Gayathrimanoj786 <108818623+Gayathrimanoj786@users.noreply.github.com>
2022-07-07 00:27:01 +0530

    c is updated as 2

--- Documents/cons/sample.cs
+++ Documents/cons/sample.cs
@@ -15,7 +15,7 @@ public class Login
         StreamReader sr = new StreamReader("D://NKK/GAYATHRI/password.txt");
         sr.BaseStream.Seek(0, SeekOrigin.Begin);
         ss1 = Convert.ToInt16(sr.ReadLine());
-        int c = 1;
+        int c = 2;
         
         while (c < ss1)
         {
